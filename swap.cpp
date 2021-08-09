0aece4140d72668f655b6f7e8f981efa3932223a
Sumit Kumar <sumitkn.pine@gmail.com>
2021-08-09 18:08:32 +0530

    swap number

---
+++ swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	int b = 10;
+
+	cout << "a & b before swap : " << a << "  " << b << endl;
+
+	swap(a,b);
+
+	cout << "a & b after swap : " << a << "  " << b << endl;
+
+	return 0;
+}
