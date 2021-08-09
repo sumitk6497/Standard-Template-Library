9d93ffc2b06dab6de398023753302fadbd3610a6
Sumit Kumar <sumitkn.pine@gmail.com>
2021-08-09 18:09:00 +0530

    swap string

---
+++ swap1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string a = "github";
+	string b = "programming";
+
+	cout << "a & b before swap : " << a << "  " << b << endl;
+
+	swap(a,b);
+
+	cout << "a & b after swap : " << a << "  " << b << endl;
+
+	return 0;
+}
